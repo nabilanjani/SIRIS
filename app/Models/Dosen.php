750b5d02e13b1edd0b272cb8fc8c419e958bd2ec
nabilanjani <nabilabetarianjani@students.undip.ac.id>
2024-12-18 21:36:03 +0700

    ya

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -9,10 +9,8 @@ class Dosen extends Model
 {
     use HasFactory;
 
-    // Define the correct table name if it's not the default plural form of the model
     protected $table = 'dosen'; 
 
-    // Add the fillable columns if necessary
     protected $fillable = ['nip', 'nama'];
 }
 
